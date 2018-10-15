b009dfd1913e03c10b602deb8ee6a3ce6578d1b9
Raman Sharma <raman.sharma0096@gmail.com>
2018-10-15 17:00:26 +0200

    Change option

--- src/algorithms/eliminationorder.h
+++ src/algorithms/eliminationorder.h
@@ -49,7 +49,7 @@ enum class Heuristic
 };
 
 // Set a heuristic from above to compute the elimination order with.
-Heuristic choose_heuristic = Heuristic::RADIUS;
+Heuristic choose_heuristic = Heuristic::DEFAULT;
 
 /*
  * Always prefer the center with the greater radius.
