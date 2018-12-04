5276156ffbb5ebe0deb3f8379e999e1a37592d38
Raman Sharma <raman.sharma0096@gmail.com>
2018-12-04 13:06:06 +0100

    Refactor

--- src/growing_balls.cpp
+++ src/growing_balls.cpp
@@ -11,38 +11,35 @@
 int
 main(int argc, char** argv)
 {
-  int numberOfFiles = 17;
-  for (int i = 1; i <= numberOfFiles; i++) {
-    if (argc < 2) {
-      std::cout << "Please provide a valid .complete.txt input file!"
-                << std::endl;
-      return 1;
-    }
-
-    std::string input_path(argv[i]);
-    growing_balls::EliminationOrder eo;
-    auto es = eo.compute_elimination_order(input_path);
-
-    std::vector<growing_balls::PointOfInterest> elimination_order;
-    for (auto& e : es) {
-      auto et = e.m_elimination_time;
-      auto poi = e.m_eliminated;
-      auto elim_p = e.m_eliminated_by;
-
-      poi.set_elimination(et, elim_p.get_osm_id());
-      elimination_order.push_back(std::move(poi));
-    }
-
-    std::string output_path = input_path;
-    if (input_path.find(".complete.txt")) {
-      // replace the old ending '.complete.txt' by .ce
-      auto pos = output_path.rfind(".complete.txt");
-      assert(pos != std::string::npos);
-      output_path.replace(pos, std::string::npos, ".ce");
-    } else {
-      output_path = output_path + ".ce";
-    }
-
-    growing_balls::IO::export_eliminationorder(output_path, elimination_order);
+  if (argc < 2) {
+    std::cout << "Please provide a valid .complete.txt input file!"
+              << std::endl;
+    return 1;
   }
+
+  std::string input_path(argv[1]);
+  growing_balls::EliminationOrder eo;
+  auto es = eo.compute_elimination_order(input_path);
+
+  std::vector<growing_balls::PointOfInterest> elimination_order;
+  for (auto& e : es) {
+    auto et = e.m_elimination_time;
+    auto poi = e.m_eliminated;
+    auto elim_p = e.m_eliminated_by;
+
+    poi.set_elimination(et, elim_p.get_osm_id());
+    elimination_order.push_back(std::move(poi));
+  }
+
+  std::string output_path = input_path;
+  if (input_path.find(".complete.txt")) {
+    // replace the old ending '.complete.txt' by .ce
+    auto pos = output_path.rfind(".complete.txt");
+    assert(pos != std::string::npos);
+    output_path.replace(pos, std::string::npos, ".ce");
+  } else {
+    output_path = output_path + ".ce";
+  }
+
+  growing_balls::IO::export_eliminationorder(output_path, elimination_order);
 }
